da171798290865fc8c7541c2500073cbf5324b2e
Fredy Velásquez <macacoloco696@gmail.com>
2024-06-21 00:07:19 -0600

    perf by

--- src/exercise/06.js
+++ src/exercise/06.js
@@ -21,9 +21,6 @@ function appReducer(state, action) {
   switch (action.type) {
     // we're no longer managing the dogName state in our reducer
     // 💣 remove this case
-    case 'TYPED_IN_DOG_INPUT': {
-      return {...state, dogName: action.dogName}
-    }
     case 'UPDATE_GRID_CELL': {
       return {...state, grid: updateGridCellState(state.grid, action)}
     }
@@ -38,8 +35,7 @@ function appReducer(state, action) {
 
 function AppProvider({children}) {
   const [state, dispatch] = React.useReducer(appReducer, {
-    // 💣 remove the dogName state because we're no longer managing that
-    dogName: '',
+
     grid: initialGrid,
   })
   return (
@@ -106,10 +102,8 @@ function Cell({row, column}) {
 Cell = React.memo(Cell)
 
 function DogNameInput() {
-  // 🐨 replace the useAppState and useAppDispatch with a normal useState here
-  // to manage the dogName locally within this component
-  const state = useAppState()
-  const dispatch = useAppDispatch()
+  
+  const [dogName, setDogName] = React.useState('')
   const {dogName} = state
 
   function handleChange(event) {
